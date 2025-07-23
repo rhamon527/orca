31e9f300fb71b16e5ab723a27ab2739fa57cfc28
rhamon527 <rhamonvieiraborges7@gmail.com>
2025-07-23 10:09:14 -0300

    Update models.py

--- models.py
+++ models.py
@@ -36,6 +36,13 @@ class Funcionario(db.Model):
     data_nascimento = db.Column(db.Date, nullable=False)
     holerites = db.relationship('Holerite', backref='funcionario', lazy=True)
 
+class Funcionario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(120), nullable=False)
+    cpf = db.Column(db.String(20), nullable=False)
+    data_nascimento = db.Column(db.Date, nullable=False)
+    obra_id = db.Column(db.Integer, db.ForeignKey('obras.id'))
+
 class Holerite(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     funcionario_id = db.Column(db.Integer, db.ForeignKey('funcionario.id'), nullable=False)
